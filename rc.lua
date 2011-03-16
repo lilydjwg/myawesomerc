fc5f1ecf23cceb78d103d04bbdea76dcb9f34dbc
依云 <lilydjwg@gmail.com>
2011-03-16 23:29:55 +0800

    QQ 的更加精确的设置
    Empathy 的设置还是不起作用

--- rc.lua
+++ rc.lua
@@ -537,8 +537,6 @@ awful.rules.rules = {
 
     { rule = { class = "Empathy" },
       properties = { tag = tags[1][6] } },
-    { rule = { class = "QQ.exe" },
-      properties = { tag = tags[1][3] } },
 }
 
 -- {{{1 Signals
@@ -574,11 +572,18 @@ client.add_signal("manage", function (c, startup)
 	    awful.placement.no_offscreen(c)
 	end
     end
-    if c.class == 'QQ.exe' and c.above then
-	naughty.notify({title="QQ广告屏蔽", text="检测到一个符合条件的窗口，标题为".. c.name .."。"})
+    -- naughty.notify({title="新窗口", text="名称为 ".. c.name .."，class 为 " .. c.class:gsub('&', '&amp;') .. " 的窗口已接受管理。", preset=naughty.config.presets.critical})
+    if c.class == 'Empathy' then
+	awful.client.movetotag(tags[mouse.screen][6], c)
     end
-    if c.class == 'QQ.exe' and c.name == '腾讯网新闻' then
-	c:kill()
+    if c.instance == 'QQ.exe' then
+	if c.name == '上线提醒' then
+	elseif c.above and not c.name:match('^%w+$') then
+	    naughty.notify({title="QQ广告屏蔽", text="检测到一个符合条件的窗口，标题为".. c.name .."。", preset=naughty.config.presets.critical})
+	    c:kill()
+	else
+	    awful.client.movetotag(tags[mouse.screen][3], c)
+	end
     end
 end)
 
