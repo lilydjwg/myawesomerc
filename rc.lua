fa9fe02451b141bd97cc3a5100f99203b6325e84
依云 <lilydjwg@gmail.com>
2011-11-26 16:25:02 +0800

    awesome 更新到 3.4.11，对应的配置文件更新

--- rc.lua
+++ rc.lua
@@ -19,6 +19,30 @@ require("menu")
 
 os.setlocale("zh_CN.UTF-8")
 
+-- {{{ Error handling
+-- Check if awesome encountered an error during startup and fell back to
+-- another config (This code will only ever execute for the fallback config)
+if awesome.startup_errors then
+    naughty.notify({ preset = naughty.config.presets.critical,
+                     title = "Oops, there were errors during startup!",
+                     text = awesome.startup_errors })
+end
+
+-- Handle runtime errors after startup
+do
+    local in_error = false
+    awesome.add_signal("debug::error", function (err)
+        -- Make sure we don't go into an endless error loop
+        if in_error then return end
+        in_error = true
+
+        naughty.notify({ preset = naughty.config.presets.critical,
+                         title = "Oops, an error happened!",
+                         text = err })
+        in_error = false
+    end)
+end
+-- }}}
 -- {{{1 Variable definitions
 -- Themes define colours, icons, and wallpapers
 theme_path = awful.util.getdir("config") .. "/theme.lua"
@@ -89,7 +113,7 @@ end
 -- {{{1 Menu
 -- Create a laucher widget and a main menu
 myawesomemenu = {
-   { "编辑配置 (&E)", editor_cmd .. " " .. awful.util.getdir("config") .. "/rc.lua" },
+   { "编辑配置 (&E)", editor_cmd .. " " .. awesome.conffile },
    { "重新加载 (&R)", awesome.restart, '/usr/share/icons/gnome/16x16/actions/stock_refresh.png' },
    { "注销 (&L)", awesome.quit },
 }
@@ -518,7 +542,7 @@ floating_apps = {
   class = {
     'MPlayer', 'Flashplayer', 'Gnome-mplayer', 'Totem',
     'Eog', 'feh', 'Display', 'Gimp', 'Gimp-2.6',
-    'Screenkey', 'TempTerm',
+    'Screenkey', 'TempTerm', 'AliWangWang',
   },
   name = {
     '文件传输', 'Firefox 首选项',
