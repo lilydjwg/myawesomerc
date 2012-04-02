20de704b311bec87a88f4647f85570df53ff4743
依云 <lilydjwg@gmail.com>
2012-04-02 14:13:20 +0800

    menu update

--- menu.lua
+++ menu.lua
@@ -1,4 +1,4 @@
- menuc8205c7636e728d448c2774e6a4a944b = {     {"Avahi SSH服务器浏览器", "/usr/bin/bssh", "/usr/share//icons/gnome/16x16/devices/network-wired.png" },
+ menuf27b59fafea3e9ffcf3f6993c1c1439e = {     {"Avahi SSH服务器浏览器", "/usr/bin/bssh", "/usr/share//icons/gnome/16x16/devices/network-wired.png" },
      {"Avahi VNC服务器浏览器", "/usr/bin/bvnc", "/usr/share//icons/gnome/16x16/devices/network-wired.png" },
      {"Dropbox", "/opt/dropbox/dropboxd", "/usr/share//icons/hicolor/16x16/apps/dropbox.png" },
      {"ELinks", "xterm -e /usr/bin/elinks ", "/usr/share//icons/gnome/16x16/mimetypes/html.png" },
@@ -7,6 +7,8 @@
      {"Google Chrome", "/opt/google/chrome/google-chrome ", "/usr/share//icons/hicolor/16x16/apps/google-chrome.png" },
      {"Opera", "/usr/bin/opera ", "/usr/share//icons/hicolor/16x16/apps/opera-browser.png" },
      {"Pidgin 互联网通讯程序", "pidgin", "/usr/share//icons/hicolor/16x16/apps/pidgin.png" },
+     {"Skype", "skype", "/usr/share/pixmaps/skype.png" },
+     {"SpiderOak", "SpiderOak", "/usr/share/pixmaps/spideroak.png" },
      {"Transmission", "transmission-gtk ", "/usr/share//icons/hicolor/16x16/apps/transmission.png" },
      {"Wicd", "/usr/bin/wicd-client", "/usr/share//icons/hicolor/16x16/apps/wicd-gtk.png" },
      {"Wuala", "wuala", "/usr/share//icons/hicolor/64x64/apps/wuala.png" },
@@ -19,19 +21,29 @@
      {"阿里旺旺", "AliWangWang", "/usr/share/pixmaps/AliWangWang.png" },
  }
 
+ menu5a0afc9dbd6c2e98769d4620d4fb5b13 = {     {"Adobe Flash Player", "flash-player-properties", "/usr/share//icons/hicolor/16x16/apps/flash-player-properties.png" },
+     {"Celestia", "celestia", "/usr/share/pixmaps/celestia.png" },
+     {"Fcitx配置工具", "fcitx-configtool", "/usr/share//icons/hicolor/16x16/apps/fcitx.png" },
+     {"IBus 设置", "ibus-setup"},
+     {"Java", "jcontrol", "/usr/share//icons/hicolor/16x16/apps/sun-jcontrol.png" },
+     {"Java Policy Settings", "policytool", "/usr/share//icons/hicolor/16x16/apps/sun-java.png" },
+     {"JavaWS", "javaws-launcher ", "/usr/share//icons/hicolor/16x16/apps/sun-javaws.png" },
+     {"Opera Widget Manager", "/usr/bin/opera-widget-manager", "/usr/share//icons/hicolor/16x16/apps/opera-widget-manager.png" },
+     {"消息和 VoIP 帐号", "empathy-accounts", "/usr/share//icons/hicolor/16x16/apps/empathy.png" },
+ }
+
  menudb53804b7d726967c58fcc8c9ca03d27 = {     {"Adobe Reader 9", "acroread ", "/usr/share/pixmaps/acroread.png" },
      {"LibreOffice ", "libreoffice ", "/usr/share//icons/hicolor/16x16/apps/libreoffice-startcenter.png" },
      {"LibreOffice Base", "libreoffice --base ", "/usr/share//icons/hicolor/16x16/apps/libreoffice-base.png" },
      {"LibreOffice Calc", "libreoffice --calc ", "/usr/share//icons/hicolor/16x16/apps/libreoffice-calc.png" },
      {"LibreOffice Draw", "libreoffice --draw ", "/usr/share//icons/hicolor/16x16/apps/libreoffice-draw.png" },
      {"LibreOffice Impress", "libreoffice --impress ", "/usr/share//icons/hicolor/16x16/apps/libreoffice-impress.png" },
-     {"LibreOffice Math", "libreoffice --math ", "/usr/share//icons/hicolor/16x16/apps/libreoffice-math.png" },
      {"LibreOffice Writer", "libreoffice --writer ", "/usr/share//icons/hicolor/16x16/apps/libreoffice-writer.png" },
      {"字典", "gnome-dictionary", "/usr/share//icons/gnome/16x16/apps/accessories-dictionary.png" },
      {"电子书阅读", "FBReader ", "/usr/share/pixmaps/FBReader.png" },
  }
 
- menue1b7cee794666669f55acead93513971 = {     {"Blender", "blender", "/usr/share//icons/hicolor/16x16/apps/blender.png" },
+ menu9be72ba165a45aaed9f950ec718475be = {     {"Blender", "blender", "/usr/share//icons/hicolor/16x16/apps/blender.png" },
      {"Dia 图表编辑器", "dia ", "/usr/share//icons/hicolor/16x16/apps/dia.png" },
      {"GNU 图像处理程序", "gimp-2.6 ", "/usr/share//icons/hicolor/16x16/apps/gimp.png" },
      {"Inkscape", "inkscape ", "/usr/share//icons/hicolor/16x16/apps/inkscape.png" },
@@ -40,9 +52,9 @@
 
  menu526fe18cc54d217db3fdb0bf6c5e9bc8 = {     {"Audacity", "audacity", "/usr/share//icons/hicolor/16x16/apps/audacity.png" },
      {"FlashPlayer", "flashplayer", "/usr/share/pixmaps/gflashplayer.png" },
-     {"PulseAudio Volume Control", "pavucontrol", "/usr/share//icons/gnome/16x16/apps/multimedia-volume-control.png" },
+     {"PulseAudio Volume Control", "pavucontrol", "/usr/share//icons/hicolor/16x16/apps/multimedia-volume-control.png" },
      {"QT V4L2 test Utility", "qv4l2", "/usr/share//icons/hicolor/16x16/apps/qv4l2.png" },
-     {"VLC media player", "vlc ", "/usr/share//icons/hicolor/16x16/apps/vlc.png" },
+     {"VLC media player", "/usr/bin/vlc ", "/usr/share//icons/hicolor/16x16/apps/vlc.png" },
      {"gtk-recordMyDesktop", "gtk-recordMyDesktop", "/usr/share/pixmaps/gtk-recordmydesktop.png" },
  }
 
@@ -53,15 +65,12 @@
      {"Narcissu2-en", "/usr/bin/narcissu2-en", "/usr/share/pixmaps/narcissu2-en.xpm" },
  }
 
- menu634d80d0a3e1eec96438b7071d9edc7f = {     {"Celestia", "celestia", "/usr/share/pixmaps/celestia.png" },
-     {"SAGE", "xterm -e /opt/sage/sage -notebook", "///opt/sage/data/extcode/notebook/images/sageicon.png" },
- }
-
  menude71492af019157e5a5ef958b570e760 = {     {"Avahi Zero浏览器", "/usr/bin/avahi-discover", "/usr/share//icons/gnome/16x16/devices/network-wired.png" },
      {"ELF Library Viewer", "elflibviewer -caption \"ELF Library Viewer\" ", "/usr/share//icons/hicolor/48x48/apps/elflibviewer.png" },
      {"Fcitx", "fcitx", "/usr/share//icons/hicolor/16x16/apps/fcitx.png" },
      {"GParted", "gparted-pkexec", "/usr/share//icons/hicolor/16x16/apps/gparted.png" },
      {"Htop", "xterm -e htop", "/usr/share/pixmaps/htop.png" },
+     {"IBus", "ibus-daemon --xim"},
      {"Oracle VM VirtualBox", "VirtualBox ", "/usr/share//icons/hicolor/16x16/mimetypes/virtualbox.png" },
      {"TrueCrypt", "truecrypt", "/usr/share/pixmaps/truecrypt.xpm" },
      {"Wireshark", "wireshark ", "/usr/share//icons/hicolor/16x16/apps/wireshark.png" },
@@ -77,9 +86,9 @@
      {"Devhelp", "devhelp", "/usr/share//icons/hicolor/16x16/apps/devhelp.png" },
      {"ELF Library Viewer", "elflibviewer -caption \"ELF Library Viewer\" ", "/usr/share//icons/hicolor/48x48/apps/elflibviewer.png" },
      {"Emacs", "emacs ", "/usr/share//icons/hicolor/16x16/apps/emacs.png" },
-     {"Java Monitoring and Management Console", "jconsole", "/usr/share/pixmaps/java.png" },
-     {"Java VisualVM", "jvisualvm", "/usr/share/pixmaps/java.png" },
-     {"Meld", "meld", "/usr/share//icons/hicolor/16x16/apps/meld.png" },
+     {"Java Monitoring and Management Console", "jconsole", "/usr/share//icons/hicolor/16x16/apps/sun-java.png" },
+     {"Java VisualVM", "jvisualvm", "/usr/share//icons/hicolor/16x16/apps/sun-java.png" },
+     {"Meld", "meld ", "/usr/share//icons/hicolor/16x16/apps/meld.png" },
      {"Qt Assistant", "/usr/bin/assistant", "/usr/share//icons/hicolor/32x32/apps/assistant.png" },
      {"Qt Designer", "/usr/bin/designer", "/usr/share//icons/hicolor/128x128/apps/designer.png" },
      {"Qt Linguist", "/usr/bin/linguist", "/usr/share//icons/hicolor/16x16/apps/linguist.png" },
@@ -89,6 +98,7 @@
 
  menuc9a6ee90f5d43732e3f6cf4dcaa8493c = {     {"7-Zip FM", "7zFM", "/usr/share//icons/hicolor/32x32/apps/p7zip.png" },
      {"FreeFileSync", "FreeFileSync"},
+     {"Keyboard", "matchbox-keyboard", "/usr/share/pixmaps/matchbox-keyboard.png" },
      {"Leafpad", "leafpad ", "/usr/share//icons/hicolor/16x16/apps/leafpad.png" },
      {"Parcellite", "parcellite", "/usr/share/pixmaps/parcellite.png" },
      {"Qalculate!", "qalculate-gtk", "/usr/share/pixmaps/qalculate.png" },
@@ -108,13 +118,13 @@
      {"终端", "gnome-terminal", "/usr/share//icons/gnome/16x16/apps/utilities-terminal.png" },
  }
 
-xdgmenu = {    {"Internet", menuc8205c7636e728d448c2774e6a4a944b},
+xdgmenu = {    {"互联网", menuf27b59fafea3e9ffcf3f6993c1c1439e},
+    {"其它", menu5a0afc9dbd6c2e98769d4620d4fb5b13},
     {"办公", menudb53804b7d726967c58fcc8c9ca03d27},
-    {"图像", menue1b7cee794666669f55acead93513971},
+    {"图形", menu9be72ba165a45aaed9f950ec718475be},
     {"影音", menu526fe18cc54d217db3fdb0bf6c5e9bc8},
     {"教育", menu8640cb1581c2ae2de5a1462405b2943b},
     {"游戏", menuba08216f13dd1742157412386eee1225},
-    {"科学和数学", menu634d80d0a3e1eec96438b7071d9edc7f},
     {"系统工具", menude71492af019157e5a5ef958b570e760},
     {"编程", menu41282bf95e43c64d579757573a03cdde},
     {"附件", menuc9a6ee90f5d43732e3f6cf4dcaa8493c},
