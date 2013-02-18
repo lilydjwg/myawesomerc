78aced3b33b7cfa50c259708648550cf6295556c
依云 <lilydjwg@gmail.com>
2013-02-18 16:40:57 +0800

    fix systray position

--- rc.lua
+++ rc.lua
@@ -398,11 +398,11 @@ for s = 1, screen.count() do
 
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
-    if s == 1 then right_layout:add(wibox.widget.systray()) end
     right_layout:add(memwidget)
     right_layout:add(batwidget)
     right_layout:add(netwidget)
     right_layout:add(volumewidget)
+    if s == 1 then right_layout:add(wibox.widget.systray()) end
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
 
